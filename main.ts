53249ca91824ace884c4214e824e585050f14db0
Daniel Gallichan <dgallichan@yahoo.com>
2023-12-07 15:24:54 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,25 @@
+function radioSendData () {
+    radio.sendValue("1", input.magneticForce(Dimension.X))
+    radio.sendValue("2", input.magneticForce(Dimension.Y))
+    radio.sendValue("3", input.magneticForce(Dimension.Z))
+    for (let index = 0; index <= extVars.length - 1; index++) {
+        radio.sendValue(convertToText(index + 4), extVars[index])
+    }
+}
+function joinAllPars () {
+    thisString = "" + input.magneticForce(Dimension.X) + "," + input.magneticForce(Dimension.Y) + "," + input.magneticForce(Dimension.Z)
+    for (let value of extVars) {
+        thisString = "" + thisString + "," + value
+    }
+    return thisString
+}
+let thisString = ""
+let extVars: number[] = []
 LSM6DSO.power(LSM6DSO.POWER_ONOFF.ON)
+radio.setGroup(1)
 basic.forever(function () {
-    serial.writeNumbers(LSM6DSO.get())
-    basic.pause(100)
+    extVars = LSM6DSO.get()
+    radioSendData()
+    basic.pause(25)
+    led.toggle(0, 0)
 })
